70ead94f19b968f7f0a68ce2d73dbcc484a152e7
Yumin Xia <me@yumin-xia.com>
2016-06-15 01:34:51 +0800

    [client] add update method and simple runner

--- client/client.py
+++ client/client.py
@@ -2,7 +2,8 @@
 import sys
 import requests
 import json
-
+import time
+import gpio
 # python3.3 is the minimal version requirement
 assert sys.version_info >= (3, 4)
 
@@ -18,5 +19,19 @@ def init():
     url = config['url']
 
 
-def notifyServer():
-    pass
+def notifyServer(status):
+    if status:
+        status = 1
+    else:
+        status = 0
+    r = requests.post(url, data={'status': status})
+    if r.status_code != 200:
+        print("error")
+
+
+if __name__ == '__main__':
+    init()
+    while True:
+        status = gpio.isDangerous()
+        notifyServer(status)
+        time.sleep(1)
