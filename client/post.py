376c9ef2202faf750d18b2ef803c415c958367d4
Yumin Xia <me@yumin-xia.com>
2016-08-26 20:29:01 +0800

    [client] add naming in post file

--- client/post.py
+++ client/post.py
@@ -8,18 +8,22 @@ assert sys.version_info >= (3, 4)
 url = ''
 data = []
 
+NAMING = ['MQ2A', 'MQ2D', 'MQ7A', 'MQ7D', 'MQ4A', 'MQ4D', 'MQ135A', 'MQ135D', 'TEMPERATURE', 'MOISTURE']
 
 def init():
     global url
     url = sys.argv[1]
     # get parameters
     global data
-    data = [float(i) for i in sys.argv[2:]]
+    data = [int(i) for i in sys.argv[2:]]
 
 
 if __name__ == '__main__':
     init()
-    dataToSend = {'MQ2': data[0], 'MQ7': data[2]}
+    dataToSend = {}
+    for i in range(data.__len__()):
+        dataToSend[NAMING[i]] = data[i]
+    print(dataToSend)
     r = requests.post(url, data=dataToSend)
     if r.status_code != 200:
         print("http error")
